e2285f37583c4fd91ad83991982b71544789fe27
Ekaterina <e.kiseleva01@mail.ru>
2022-03-27 15:49:39 +0300

    Добавлена функция тестирования

--- src/main/java/com/java/laba3/Program.java
+++ src/main/java/com/java/laba3/Program.java
@@ -3,18 +3,78 @@ package com.java.laba3;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.awt.Dimension;
+import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-
+// через интерфейс лист
 
 public class Program extends JFrame {
+
+    /**
+     * Функция, тестирующая методы
+     * @param list - тестируемый лист
+     * @param count - количество повторений (элементов)
+     * @param method - название метода
+     * */
+    public static String testMethod(List list, int count, String method)
+    {
+        long start, finish;
+        switch (method)
+        {
+            case "add":
+            {
+                start = System.nanoTime();
+                for(int i=0; i<1000;i++)
+                {
+                    list.add(5);
+                }
+                finish = System.nanoTime();
+                break;
+            }
+            case "get":
+            {
+                start = System.nanoTime();
+                for(int i=0; i<1000;i++)
+                {
+                    list.get(i);
+                }
+                finish = System.nanoTime();
+                break;
+            }
+            case "set":
+            {
+                start = System.nanoTime();
+                for(int i=0; i<1000;i++)
+                {
+                    list.set(i, 0);
+                }
+                finish = System.nanoTime();
+                break;
+            }
+            case "remove":
+            {
+                start = System.nanoTime();
+                for(int i=0; i<1000;i++)
+                {
+                    list.remove(0);
+                }
+                finish = System.nanoTime();
+                break;
+            }
+            default:
+                return "Ошибка";
+        }
+        return Long.toString(finish - start);
+    }
+
+
     /**
      * Стартовая точка
      * @param args
      * */
     public static void main(String[] args) {
-        JFrame frame = new JFrame(" ");
+        JFrame frame = new JFrame("Сравнение методов");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Название колонок в таблице
         String[] columnNames = {
@@ -29,13 +89,6 @@ public class Program extends JFrame {
                 {"Get", "", "", "1000"},
                 {"Set", "", "", "1000"},
                 {"Delete", "", "", "1000"},
-                /*{"", "", "", ""},
-                {"", "", "", ""},
-                {"", "", "", ""},
-                {"", "", "", ""},
-                {"", "", "", ""},
-                {"", "", "", ""},*/
-
         };
 
         //Переменные, с помощью которых засекается время
@@ -44,7 +97,7 @@ public class Program extends JFrame {
         //Тестируемые листы
         ArrayList<Integer> arrayList = new ArrayList<>();
         LinkedList<Integer> linkedList = new LinkedList<>();
-
+        /*
         //Метод add для ArrayList
         start = System.nanoTime();
         for(int i=0; i<1000;i++)
@@ -53,6 +106,8 @@ public class Program extends JFrame {
         }
         finish = System.nanoTime();
         data[0][1] = Long.toString(finish - start);
+
+
         //Метод add для LinkedList
         start = System.nanoTime();
         for(int i=0; i<1000;i++)
@@ -112,7 +167,15 @@ public class Program extends JFrame {
         }
         finish = System.nanoTime();
         data[3][2] = Long.toString(finish - start);
-
+        */
+        data[0][1] = testMethod(arrayList, 1000, "add");
+        data[1][1] = testMethod(arrayList, 1000, "get");
+        data[2][1] = testMethod(arrayList, 1000, "set");
+        data[3][1] = testMethod(arrayList, 1000, "remove");
+        data[0][2] = testMethod(linkedList, 1000, "add");
+        data[1][2] = testMethod(linkedList, 1000, "get");
+        data[2][2] = testMethod(linkedList, 1000, "set");
+        data[3][2] = testMethod(linkedList, 1000, "remove");
         //Создание таблицы для предоставления результатов тестирования
         JTable table = new JTable(data, columnNames);
         JScrollPane scrollPane = new JScrollPane(table);
